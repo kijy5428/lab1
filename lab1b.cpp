4e84a873202f6b5f820463644c26e4d7e90e5bc7
jimmy <jimmy@yahoo.com.tw>
2016-03-07 00:49:39 +0800

    lab1(b) assignment

---
+++ lab1b.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+	int n;
+	
+	cout << "Please Enter Your Number Here : "  ;
+	cin  >> n;
+	cout << n;
+
+	while(n!=1){
+		
+		if (n%2==1)
+			n = 3*n +1;
+
+		else 
+			n = n/2;
+
+
+		cout << " " << n;
+
+
+	}
+
+
+	return 0;
+
+
+
+
+
+
+
+
+
+
+}
---
+++ readme.txt
@@ -0,0 +1,11 @@
+this program can be compiled and run ok
+
+input : a random positive integer is required
+
+output : a sequence of numbers  matching   with   lab1's 
+		 requirements
+
+compile g++ -o lab1b lab1b.cpp
+		./lab1b
+
+  
